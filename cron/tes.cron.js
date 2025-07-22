15a1ea5110cc589536be8e392c1acee428621319
sahdan17 <sahdan.wira.17@gmail.com>
2025-07-22 21:59:44 +0700

    Update tes.cron.js

--- cron/tes.cron.js
+++ cron/tes.cron.js
@@ -1,7 +1,9 @@
+import moment from 'moment-timezone'
 import cron from 'node-cron'
 
 const tes = () => {
-    console.log('tes cron')
+    const timestamp = moment().tz('Asia/Jakarta').format('YYYY-MM-DD HH:mm:ss')
+    console.log(`tes cron ${timestamp}`)
 }
 
 cron.schedule('* * * * *', () => {
